20e0f156692863582b25d0aee6fe2812a7cdef5f
wlandau-lilly <will.landau@lilly.com>
2019-07-04 19:22:37 -0400

    Move more stuff to the plan

--- _drake.R
+++ _drake.R
@@ -3,5 +3,7 @@ source("code/02-reading_data.R")
 source("code/plan.R")
 
 drake_config(
-  plan
+  plan,
+  memory_strategy = "memory", # will become "autoclean" next release
+  console_log_file = "drake.log"
 )
--- code/plan.R
+++ code/plan.R
@@ -2,19 +2,8 @@ data_path <- here("data", "MicrodatosCP_NV_per_nacional_3VAR.txt")
 labels_path <- here("data", "variable_labels.xls")
 processed_path <- here("output", "microdatos_censo.rds")
 
-# If file hasn't been processed, do it. This reads the raw
-# txt file and saves it as .rds file because the preprocessing
-# from census2010 function takes up a lot of time. By saving it
-# we save all the time of preprocessing columns.
-if (!file.exists(processed_path)) write_census(data_path, processed_path)
-message("Data was downloaded successfully")
-
-message("Reading the data (heavy, takes up to a few minutes)")
-read_data <- read_rds(processed_path)
-message("Data read successfully in memory")
-
-message("Drake started the plan. This is where the memory allocation error happens")
-plan <-
-  drake_plan(
-    process_data = select(read_data, c("CPRO", "CMUN", "IDHUECO"))
-  )
+plan <- drake_plan(
+  download_step = write_census(file_out(!!data_path), file_out(!!processed_path)),
+  downloaded_data = readRDS(file_in(!!processed_path)),
+  processed_data = select(downloaded_data, c("CPRO", "CMUN", "IDHUECO"))
+)
