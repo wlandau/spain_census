791aebd95ccf5fd0a0dd846be220c54e0b4164de
Jorge Cimentada <cimentadaj@gmail.com>
2019-07-04 22:55:56 +0200

    A bit of tidying before the reprex of drake problem

--- code/02-reading_data.R
+++ code/02-reading_data.R
@@ -1,8 +1,13 @@
 write_census <- function(file_path, save_path) {
-  # Download the Spanish data
-  ## data_link <- "ftp://www.ine.es/temas/censopv/cen11/Microdatos_personas_nacional.zip"
-  ## download.file(data_link, destfile = "./data/raw_data.zip")
-  ## unzip("./data/raw_data.zip", exdir = "./data/")
+  ## Download the Spanish data
+
+  if (!file.exist(file_path)) {
+    message("The data is being downloaded")
+    
+    data_link <- "ftp://www.ine.es/temas/censopv/cen11/Microdatos_personas_nacional.zip"
+    download.file(data_link, destfile = "./data/raw_data.zip")
+    unzip("./data/raw_data.zip", exdir = "./data/")
+  }
 
   message("The microdatos file is being read. This can take some time but",
           "this is only run if the preprocessed file is not saved. Once this",
@@ -13,7 +18,7 @@ write_census <- function(file_path, save_path) {
       file_path
     )
 
-  # Because this data is so big, we save it to avoid rerunning censo2019
+  # Because this data is so big, we save it to avoid rerunning censo2010
   # that way we save all the proprocessing of columns (which takes up
   # a lot of time).
   write_rds(censo, save_path)
